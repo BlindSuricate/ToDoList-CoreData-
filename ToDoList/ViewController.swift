f8d72508f3c77d2a0be263453ffbf2b09b14b9d3
Tarasenko Serhii <blindsuricate@gmail.com>
2020-05-04 15:13:50 +0300

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by user on 2020-05-04.
+//  Copyright © 2020 TarasenkoSerhii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
